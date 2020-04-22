00a2e71d4b9a0a19007074ff4c98eaf58ede8869
Nicholas Vogt <nicholasvogt@Nicholass-MacBook-Pro.local>
2020-04-21 23:08:21 -0400

    set up clearDB in pdo

--- pdo.php
+++ pdo.php
@@ -7,7 +7,7 @@
     $pdo = new PDO('mysql:host=localhost;port=8888;dbname=Oregon_Trail','Nick','Ike');
   } else {
     $isLocal = false;
-    $pdo = new PDO('mysql:host=us-cdbr-iron-east-02.cleardb.net;port=3306;dbname=heroku_9f89bb0196fa398','bb859affb4aa30','*passwrd_goes_here*');
+    $pdo = new PDO('mysql:us-cdbr-iron-east-01.cleardb.net;port=3306;dbname=heroku_a1c8419498096ac','446f56c4','*passwrd_goes_here*');
   };
 
 ?>

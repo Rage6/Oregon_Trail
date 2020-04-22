2566b83ba2427a35ad61429346e0f78710543fec
Nicholas Vogt <nicholasvogt@Nicholass-MacBook-Pro.local>
2020-04-21 23:47:34 -0400

    includes error message now

--- pdo.php
+++ pdo.php
@@ -10,4 +10,6 @@
     $pdo = new PDO('mysql:us-cdbr-iron-east-01.cleardb.net;port=3306;dbname=heroku_a1c8419498096ac','b69578920b8ccd','');
   };
 
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 ?>
